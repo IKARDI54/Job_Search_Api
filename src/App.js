18674f53dd145859c2ad19c580b630f8c7531834
Israel <maccario5@hotmail.com>
2019-10-13 21:17:20 +0200

    subida de la prueba

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Jobs from './Jobs'
 
 function App() {
   return (
@@ -16,8 +17,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn fetch
+
         </a>
+        <Jobs/>
       </header>
     </div>
   );
---
+++ src/jobs.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+class Jobs extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      jobs: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jobs.github.com/positions.json', {
+        headers: [
+          ["Content-Type", "application/json"],
+          ["Content-Type", "text/plain"]
+        ],
+      })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            jobs: result
+          });
+        },
+        // error handler
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+
+    const { error, isLoaded, jobs } = this.state;
+
+    if (error) {
+      return (
+        <div className="col">
+          Error: {error.message}
+        </div>
+      );
+    } else if (!isLoaded) {
+      return (
+        <div className="col">
+          Loading...
+        </div>
+      );
+    } else {
+      return (
+        <div className="col">
+          <table>
+              <tr>
+                <th>Title</th>
+                <th>Location</th>
+              </tr>
+            { jobs.map( h => (
+              <tr>
+                <td>{h.title}</td>
+                <td>{h.location}</td>
+              </tr>
+            ))}
+          </table>
+        </div>
+      );
+    }
+  }
+}
+
+export default Jobs;
